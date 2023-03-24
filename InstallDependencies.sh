68685c2e3042132ee40cb923d99c0c87417d4388
jonathan zollinger <jonathan.zollinger@gmail.com>
2023-03-24 09:16:32 -0600

    wip: list dependences to install

---
+++ InstallDependencies.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo yum -y cmake charm gum glow gcc gcc-c++ nodejs waybar rofi
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
